b4c294298a3c76c516a13fa10dac87e7f331bdf9
Coding SA <codingwebsa@gmail.com>
2023-06-23 20:49:17 +0600

    fixed bugs

--- app/newsletter.tsx
+++ app/newsletter.tsx
@@ -14,21 +14,6 @@ import { NewsletterIcon } from "@/components/icons";
 const Newsletter = () => {
   return (
     <>
-      <button className="group mx-auto flex w-fit items-center gap-2 rounded border border-white/10 bg-white/5 px-6 py-3 shadow-lg">
-        <span>
-          <NewsletterIcon size={20} />
-        </span>
-        {/* hidden in mobile */}
-        <p className="hidden text-base underline transition-colors duration-200 group-hover:text-white/80 md:block">
-          Subscribe to our newsletter to stay updated with all the newest AI
-          tools.
-        </p>
-        {/* hidden in (md) */}
-        <p className="block text-sm md:hidden">
-          Subscribe to our newsletter To stay up to date.
-        </p>
-      </button>
-
       <Dialog>
         <DialogTrigger>
           <button className="group flex w-fit items-center gap-2 rounded border border-white/10 bg-white/5 px-6 py-3 shadow-lg">
